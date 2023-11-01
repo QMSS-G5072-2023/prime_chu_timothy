c745dba35bfac9ea4ac2a2e70d72dedd558f5f95
t1mmy89ru <56981409+t1mmy89ru@users.noreply.github.com>
2023-11-01 10:32:52 -0400

    test function

--- tests/test_prime_tjc2188.py
+++ tests/test_prime_tjc2188.py
@@ -1,1 +1,9 @@
+import math
+
 from prime_tjc2188 import prime_tjc2188
+
+def test_is_prime():
+    assert prime_tjc2188.is_prime(3) is True, "This is prime" 
+    assert prime_tjc2188.is_prime(7) is True,  "This is prime"  
+    assert prime_tjc2188.is_prime(6) is False, "This is not prime"  
+    assert prime_tjc2188.is_prime(9) is False, "This is not prime" 
