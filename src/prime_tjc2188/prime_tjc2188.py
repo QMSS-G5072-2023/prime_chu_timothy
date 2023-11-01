c4ca5a7526661d5f505aa2ee913b70fe686d09b3
t1mmy89ru <56981409+t1mmy89ru@users.noreply.github.com>
2023-11-01 00:38:34 -0400

    add function

---
+++ src/prime_tjc2188/prime_tjc2188.py
@@ -0,0 +1,31 @@
+import math
+    """
+    Returns True if intiger is prime and False if intiger is not prime.
+
+    Parameters
+    ----------
+    n: intiger
+
+    Returns
+    -------
+    True
+    False
+    
+    Examples
+    --------
+    >>> from prime_tjc2188 import prime
+    >>> n = 5
+    >>> prime_tjc2188.prime(n)
+    True
+    """
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+
+
